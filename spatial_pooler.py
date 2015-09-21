6bcacdcf875d7ec08b549f78f50071a06743de36
flux7 <jocelyn@flux7.com>
2015-09-21 07:59:44 -0700

    removed addNoise

--- spatial_pooler.py
+++ spatial_pooler.py
@@ -67,29 +67,6 @@ class Spooler(object):
 
 	    print self.activeArray.nonzero()
 
-
-  	def addNoise(self, noiseLevel):
-	    """Flip the value of 10% of input bits (add noise)
-
-	    :param noiseLevel: The percentage of total input bits that should be flipped
-	    """
-
-	    for _ in range(int(noiseLevel * self.inputSize)):
-	      # 0.1*self.inputSize represents 10% of the total input bits
-	      # random.random() returns a float between 0 and 1
-	      randomPosition = int(random() * self.inputSize)
-
-	      # Flipping the bit at the randomly picked position
-	      if self.inputArray[randomPosition] == 1:
-	        self.inputArray[randomPosition] = 0
-
-	      else:
-	        self.inputArray[randomPosition] = 1
-
-	      # Uncomment the following line to know which positions had been flipped.
-	      # print "The value at " + str(randomPosition) + " has been flipped"
-
-
 img = Image.open('img/parrot.png').convert('1')
 arr = np.array(img)
 inputVector = arr.astype(int)
