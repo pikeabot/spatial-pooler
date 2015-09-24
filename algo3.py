883265129e2b2ae4dec27f8bbea1ce6e16b13717
flux7 <jocelyn@flux7.com>
2015-09-24 16:25:12 -0700

    updated augmented spatial pooler script

--- algo3.py
+++ algo3.py
@@ -24,7 +24,8 @@
 import numpy
 from random import randrange, random
 #from nupic.bindings.algorithms import SpatialPooler
-from nupic.research.spatial_pooler import SpatialPooler
+#from nupic.research.spatial_pooler import SpatialPooler
+from spatial_pooler import SpatialPooler
 import Image
 from nupic.bindings.math import (SM32 as SparseMatrix,
                                  SM_01_32_32 as SparseBinaryMatrix,
@@ -38,113 +39,189 @@ VERSION=2
 class AugmentedSpatialPooler(SpatialPooler):
 	def __init__(self, inputDimensions, columnDimensions):
 	    SpatialPooler.__init__(self, inputDimensions, columnDimensions)
-        '''
-	    inputDimensions = numpy.array((16,16), ndmin=1)
-	    columnDimensions = numpy.array((16,16), ndmin=1)
 
-	    numInputs = inputDimensions.prod()
-	    numColumns = columnDimensions.prod()
-	    self._numInputs = int(numInputs)
-	    self._numColumns = int(numColumns)
-	    self._columnDimensions = columnDimensions
-	    self._inputDimensions = inputDimensions	    
-	    #self._numColumns = numColumns	
-	    #self._numInputs = numInputs
-	    self._connectedSynapses = SparseBinaryMatrix(numInputs)
-	    self._connectedSynapses.resize(numColumns, numInputs)
-	    self._boostFactors = numpy.ones(numColumns, dtype=realDType)
-	    self._inhibitionRadius = 0
-	    self._updateInhibitionRadius()
+	def _calculateMeanInput(self, inputVector):
+		return numpy.ndarray.mean(inputVector)
 
-	    self._potentialPools = SparseBinaryMatrix(numInputs)
-	    self._potentialPools.resize(numColumns, numInputs)
-	    self._synPermTrimThreshold = .05 / 2.0
-	    self._potentialRadius = int(min(16, numInputs))
-	    self._potentialPct = .5
-	    self._globalInhibition = False
-	    self._numActiveColumnsPerInhArea = int(10.0)
-	    self._localAreaDensity = -1.0
-	    self._stimulusThreshold = 0
-	    self._synPermInactiveDec = 0.008
-	    self._synPermActiveInc = 0.05
-	    self._synPermBelowStimulusInc = 0.10 / 10.0
-	    self._synPermConnected = 0.10
-	    self._minPctOverlapDutyCycles = 0.001
-	    self._minPctActiveDutyCycles = 0.001
-	    self._dutyCyclePeriod = 1000
-	    self._maxBoost = 10.0
-	    self._spVerbosity = 0
-	    self._wrapAround = True
-	    self._synPermMin = 0.0
-	    self._synPermMax = 1.0
-
-	    #self._updateInhibitionRadius()
-	    self._tieBreaker = 0.01*numpy.array([self._random.getReal64() for i in
-                                        xrange(self._numColumns)])
-
-	    if self._synPermTrimThreshold >= self._synPermConnected:
-	      raise InvalidSPParamValueError(
-	        "synPermTrimThreshold ({}) must be less than synPermConnected ({})"
-	        .format(repr(self._synPermTrimThreshold),
-	                repr(self._synPermConnected)))
-
-	    self._updatePeriod = 50
-	    initConnectedPct = 0.5
-	    self._version = VERSION
-	    self._iterationNum = 0
-	    self._iterationLearnNum = 0
-	    self._permanences = SparseMatrix(numColumns, numInputs)
-	    self._connectedCounts = numpy.zeros(numColumns, dtype=realDType)
-
-	    # Initialize the set of permanence values for each column. Ensure that
-	    # each column is connected to enough input bits to allow it to be
-	    # activated.
-	    for columnIndex in xrange(numColumns):
-	        potential = self._mapPotential(columnIndex, wrapAround=self._wrapAround)
-	        self._potentialPools.replaceSparseRow(i, potential.nonzero()[0])
-	        perm = self._initPermanence(potential, initConnectedPct)
-	        self._updatePermanencesForColumn(perm, columnIndex, raisePerm=True)
-
-	    self._overlapDutyCycles = numpy.zeros(numColumns, dtype=realDType)
-	    self._activeDutyCycles = numpy.zeros(numColumns, dtype=realDType)
-	    self._minOverlapDutyCycles = numpy.zeros(numColumns,
-	                                             dtype=realDType)
-	    self._minActiveDutyCycles = numpy.zeros(numColumns,
-	                                            dtype=realDType)
-	    self._boostFactors = numpy.ones(numColumns, dtype=realDType)
-
-	    # The inhibition radius determines the size of a column's local
-	    # neighborhood.  A cortical column must overcome the overlap score of
-	    # columns in its neighborhood in order to become active. This radius is
-	    # updated every learning round. It grows and shrinks with the average
-	    # number of connected synapses per column.
-	    self._inhibitionRadius = 0
-	    self._updateInhibitionRadius()
-
-	    if self._spVerbosity > 0:
-	      self.printParameters()
-	    super(AugmentedSpatialPooler, self).__init__()
-	    
-
-	def _calculateOverlap(self, inputVector):
+	def _adaptSynapses2(self, inputVector, activeColumns):
 	    """
-	    This function determines each column's overlap with the current input
-	    vector. The overlap of a column is the number of synapses for that column
-	    that are connected (permanence value is greater than '_synPermConnected')
-	    to input bits which are turned on. Overlap values that are lower than
-	    the 'stimulusThreshold' are ignored. The implementation takes advantage of
-	    the SparseBinaryMatrix class to perform this calculation efficiently.
+	    The primary method in charge of learning. Adapts the permanence values of
+	    the synapses based on the input vector, and the chosen columns after
+	    inhibition round. Permanence values are increased for synapses connected to
+	    input bits that are turned on, and decreased for synapses connected to
+	    inputs bits that are turned off.
+
 	    Parameters:
 	    ----------------------------
-	    @param inputVector: a numpy array of 0's and 1's that comprises the input to
-	                    the spatial pooler.
+	    @param inputVector:
+	                    A numpy array of 0's and 1's that comprises the input to
+	                    the spatial pooler. There exists an entry in the array
+	                    for every input bit.
+	    @param activeColumns:
+	                    An array containing the indices of the columns that
+	                    survived inhibition.
+	    """
+	    meanInput = self._calculateMeanInput(inputVector)
+	    #get indicies where the input synapse is greater than the mean average 
+	    #check for if they are greater than connectThreshold will be done in 
+	    #updatePermanencesForColumn
+	    hiInputIndices = numpy.where((inputVector > meanInput) & (self._permanences >=self._synPermConnected))[0]
+	    loInputIndices = numpy.where((inputVector > meanInput) & (self._permanences <self._synPermConnected))[0]
+	    #create default array of permanence value changes
+	    permChanges = numpy.zeros(self._numInputs)
+	    permChanges2 = numpy.zeros(self._numInputs)
+	    #fill array with default permanence values for inactive synapses
+	    permChanges.fill(-1 * self._synPermInactiveDec)
+	    permChanges2.fill(-1 * self._synPermInactiveDec)
+	    #update permanence changes array with inputs above the meanInput
+	    permChanges[loInputIndices] = self._synPermActiveInc
+	    permChanges2[hiInputIndices] = self._synPermActiveInc
+	    for i in activeColumns:
+	        perm = self._permanences.getRow(i)
+	        perm2 = self._permanences.getRow(i)
+	        maskPotential = numpy.where(self._potentialPools.getRow(i) > 0)[0]
+	        perm[maskPotential] += permChanges[maskPotential]
+	        perm2[maskPotential] += permChanges2[maskPotential]
+	        self._updatePermanencesForColumn(perm, i, raisePerm=True)
+	        self._updatePermanencesForColumn2(perm2, i, meanInput, raisePerm=True)
+
+	def _bumpUpWeakColumns(self):
+	    """
+	    This method increases the permanence values of synapses of columns whose
+	    activity level has been too low. Such columns are identified by having an
+	    overlap duty cycle that drops too much below those of their peers. The
+	    permanence values for such columns are increased.
+	    """
+	    weakColumns = numpy.where(self._overlapDutyCycles
+	                                < self._minOverlapDutyCycles)[0]
+	    for i in weakColumns:
+	        perm = self._permanences.getRow(i).astype(realDType)
+	        maskPotential = numpy.where(self._potentialPools.getRow(i) > 0)[0]
+	        perm[maskPotential] += self._synPermBelowStimulusInc
+	        self._updatePermanencesForColumn(perm, i, raisePerm=False)
+
+	def _raisePermanenceToThreshold(self, perm, mask):
+	    """
+	    This method ensures that each column has enough connections to input bits
+	    to allow it to become active. Since a column must have at least
+	    'self._stimulusThreshold' overlaps in order to be considered during the
+	    inhibition phase, columns without such minimal number of connections, even
+	    if all the input bits they are connected to turn on, have no chance of
+	    obtaining the minimum threshold. For such columns, the permanence values
+	    are increased until the minimum number of connections are formed.
+
+
+	    Parameters:
+	    ----------------------------
+	    @param perm:    An array of permanence values for a column. The array is
+	                    "dense", i.e. it contains an entry for each input bit, even
+	                    if the permanence value is 0.
+	    @param mask:    the indices of the columns whose permanences need to be
+	                    raised.
+	    """
+	    if len(mask) < self._stimulusThreshold:
+	        raise Exception("This is likely due to a " +
+	        "value of stimulusThreshold that is too large relative " +
+	        "to the input size. [len(mask) < self._stimulusThreshold]")
+
+	    numpy.clip(perm, self._synPermMin, self._synPermMax, out=perm)
+	    while True:
+	        numConnected = numpy.nonzero(perm > self._synPermConnected)[0].size
+	        if numConnected >= self._stimulusThreshold:
+	            return
+	        perm[mask] += self._synPermBelowStimulusInc
+
+	def _updatePermanencesForColumn2(self, perm, index, meanInput, raisePerm=True):
+	    """
+	    This method updates the permanence matrix with a column's new permanence
+	    values. The column is identified by its index, which reflects the row in
+	    the matrix, and the permanence is given in 'dense' form, i.e. a full
+	    array containing all the zeros as well as the non-zero values. It is in
+	    charge of implementing 'clipping' - ensuring that the permanence values are
+	    always between 0 and 1 - and 'trimming' - enforcing sparsity by zeroing out
+	    all permanence values below '_synPermTrimThreshold'. It also maintains
+	    the consistency between 'self._permanences' (the matrix storing the
+	    permanence values), 'self._connectedSynapses', (the matrix storing the bits
+	    each column is connected to), and 'self._connectedCounts' (an array storing
+	    the number of input bits each column is connected to). Every method wishing
+	    to modify the permanence matrix should do so through this method.
+
+	    Parameters:
+	    ----------------------------
+	    @param perm:    An array of permanence values for a column. The array is
+	                    "dense", i.e. it contains an entry for each input bit, even
+	                    if the permanence value is 0.
+	    @param index:   The index identifying a column in the permanence, potential
+	                    and connectivity matrices
+	    @param raisePerm: A boolean value indicating whether the permanence values
+	                    should be raised until a minimum number are synapses are in
+	                    a connected state. Should be set to 'false' when a direct
+	                    assignment is required.
 	    """
 
-	    overlaps = numpy.zeros(self._numColumns).astype(realDType)
-	    self._connectedSynapses.rightVecSumAtNZ_fast(inputVector, overlaps)
-	    overlaps[overlaps < self._stimulusThreshold] = 0
-	    return overlaps
-	    '''
+	    maskPotential = numpy.where(self._potentialPools.getRow(index) > 0)[0]
+	    if raisePerm:
+	        self._raisePermanenceToThreshold(perm, maskPotential)
+	    perm[perm < self._synPermTrimThreshold] = 0
+	    numpy.minimum(perm+self._synPermActiveInc, self._synPermConnected-self._synPermActiveInc, out=perm)
+	    newConnected = numpy.where(perm >= self._synPermConnected)[0]
+	    self._permanences.setRowFromDense(index, perm)
+	    self._connectedSynapses.replaceSparseRow(index, newConnected)
+	    self._connectedCounts[index] = newConnected.size
+
+	def _updateBoostFactors2(self):
+	    """
+	    Update the boost factors for all columns. The boost factors are used to
+	    increase the overlap of inactive columns to improve their chances of
+	    becoming active. and hence encourage participation of more columns in the
+	    learning process. This is a line defined as: y = mx + b boost =
+	    (1-maxBoost)/minDuty * dutyCycle + maxFiringBoost. Intuitively this means
+	    that columns that have been active enough have a boost factor of 1, meaning
+	    their overlap is not boosted. Columns whose active duty cycle drops too much
+	    below that of their neighbors are boosted depending on how infrequently they
+	    have been active. The more infrequent, the more they are boosted. The exact
+	    boost factor is linearly interpolated between the points (dutyCycle:0,
+	    boost:maxFiringBoost) and (dutyCycle:minDuty, boost:1.0).
+
+	            boostFactor
+	                ^
+	    maxBoost _  |
+	                |\
+	                | \
+	          1  _  |  \ _ _ _ _ _ _ _
+	                |
+	                +--------------------> activeDutyCycle
+	                   |
+	            minActiveDutyCycle
+	    """
+	    #get boost factor indicies for columns where the boost factor is greater than the
+	    #max boost
+	    mask = numpy.where(self._boostFactors > self._maxBoost)[0]
+
+	    #get columns where activity is less than min activity
+	    self._boostFactors[(self._activeDutyCycles >self._minActiveDutyCycles)]
+	    #set column boost values to 1 where activity(c) < minActivity (c) and boost(c) > max boost
+	    self._boostFactors[mask] = 1.0
+
+	    #maxPerm = self._permanences.max()[2]
+
+	    #loop through columns
+	    for i in range(0, self._numColumns):
+	        perm = self._permanences.getRow(i)
+	        #get indicies of disconnected synapses
+	        disconnectedIndicies = numpy.where(self._synPermConnected > perm)[0]
+	        #get the max value of the set of disconnected indicies
+	        maxPerm = numpy.ndarray.max(disconnectedIndicies)
+	        #get the array of disconnected synapses
+	        disconnected = perm[disconnectedIndicies]
+	        #get syanpses where the permanence of the synapse is greater than the max permanence
+	    	resetPerm = numpy.where(disconnected > maxPerm)
+	    	#get closest synapse
+	    	maxS = resetPerm[0]
+	    	#set the synapse permanence to the max synapse
+	    	perm[maxS] = self._synPermConnected + self._synPermActiveInc
+	    	
+
 	def compute2(self, inputVector, learn, activeArray):
 	    """
 	    This is the primary public method of the SpatialPooler class. This
@@ -191,12 +268,12 @@ class AugmentedSpatialPooler(SpatialPooler):
 
 	    # Apply inhibition to determine the winning columns
 	    activeColumns = self._inhibitColumns(boostedOverlaps)
-	    print activeColumns.shape
+
 	    if learn:
-		    self._adaptSynapses(inputVector, activeColumns)
+		    self._adaptSynapses2(inputVector, activeColumns)
 		    self._updateDutyCycles(overlaps, activeColumns)
 		    self._bumpUpWeakColumns()
-		    self._updateBoostFactors()
+		    self._updateBoostFactors2()
 		    if self._isUpdateRound():
 		        self._updateInhibitionRadius()
 		        self._updateMinDutyCycles()
@@ -235,9 +312,10 @@ def run(img_list):
     	#compute calculates the overlap, inhibits the columns and then runs the learn
     	#synapse connection method
     	#it then updates the activeArray
+
     	asp.compute2(img.ravel(), True, activeArray)
 
-    	print activeArray.nonzero()
+    print activeArray.nonzero()
 
 asp = AugmentedSpatialPooler((16,16), (16,16))
 #in this example I read in a greyscale image and then take 16px x 16px samples from 
@@ -253,7 +331,7 @@ img_list=[]
 #create a list of the sample patches to make the sets easy to run through
 img_list.append(inputVector[0:16, 1:17])
 img_list.append(inputVector[125:141, 100:116])
-img_list.append(inputVector[213:229, 44:60])
+img_list.append(inputVector[153:169, 44:60])
 
 for i in range(0,3):
 	print "current loop: " + str(i)
